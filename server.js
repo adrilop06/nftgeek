c2e89e856e7d03cbf0914c1857fc664fc681af93
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-08 04:00:07 -0500

    commit

--- server.js
+++ server.js
@@ -59,7 +59,9 @@ var corsOptionsDelegate = function (req, callback) {
 //app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   });
 // app.options('*', cors(corsOptionsDelegate))
