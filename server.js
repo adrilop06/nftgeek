5a8028790a547a565e2854b6e991ebfec9eabdc0
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-08 05:14:14 -0500

    commit

--- server.js
+++ server.js
@@ -47,14 +47,14 @@ app.use(express.json());
 
 
 //use cors
-app.use(cors());
-/*app.use(function(req, res, next) {
+//app.use(cors());
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET ,PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
-  });*/
+  });
 // app.options('*', cors(corsOptionsDelegate))
 //registration user process. Post and update user information
 //route user
