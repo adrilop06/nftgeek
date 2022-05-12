7ffdccc273f0dfdf89a2ac98a87432ad4318435d
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-12 10:46:39 -0500

    commit

--- Route/Posts/postRoute.js
+++ Route/Posts/postRoute.js
@@ -35,7 +35,9 @@ postRoute.use(function(req, res, next) {
 //postRoute.use(cors());
 postRoute.options('*', cors());
 //create a post
-postRoute.post('/', cors(), authorizationMiddleware, fileImages.single("image"), imgResize, createPostController);
+postRoute.post('/', cors(), authorizationMiddleware, fileImages.single("image"), imgResize, createPostController, function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+});
 //likes post
 postRoute.put('/likes', authorizationMiddleware, likePostController);
 //likes post
