4497e61b6537556af4cc4a3bf550ebbd4f19a1f5
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-18 13:19:44 -0500

    commit

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -6,6 +6,9 @@ const dbConnect = async () => {
         await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+       
             
         })
         console.log('Data Base is connected');
