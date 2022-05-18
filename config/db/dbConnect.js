4893216858f3da5f3f81e94558b9b0a9fa324378
Adrian Lopez <adrianlopezpazos@gmail.com>
2022-05-18 18:59:01 -0500

    commit

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -3,15 +3,7 @@ const mongoose = require ('mongoose');
 //arrow function that try to connect with data base and catch the error
 const dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            
-            
-            
-        })
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log('Data Base is connected');
     }catch (error){
         console.log('Data Bse not connected. Error: ' + error.message);
